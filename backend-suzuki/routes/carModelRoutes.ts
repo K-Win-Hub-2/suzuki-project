e0f40a8f3b3a6a36e57ac9c76f08dcbebbde4ae2
Oakar-Kyaw <oakarkyaw7090@gmail.com>
2024-09-24 14:22:10 +0630

    clear admin route

--- backend-suzuki/routes/carModelRoutes.ts
+++ backend-suzuki/routes/carModelRoutes.ts
@@ -6,7 +6,7 @@ import catchError from "../lib/catchError"
 import { createCarModel, deleteCarModel, listAllCarModel, readCarModel, updateCarModel } from "../controllers/carModelController"
 module.exports = (app: Express): void =>{
     app.route("/api/v1/car-models")
-        .get(verifyToken, checkAdminType, catchError(listAllCarModel))
+        .get(verifyToken, catchError(listAllCarModel))
         .post(verifyToken, S3UploadImage.single("car_model"), checkAdminType, catchError(createCarModel))
 
     app.route("/api/v1/car-model/:id")
