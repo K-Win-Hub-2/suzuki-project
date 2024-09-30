5556c9d8856faa1974d322252fb6b56c70068046
Oakar-Kyaw <oakarkyaw7090@gmail.com>
2024-09-30 11:17:52 +0630

    add dealer

--- backend-suzuki/services/excelService.ts
+++ backend-suzuki/services/excelService.ts
@@ -3,22 +3,31 @@ import mongoose, { FilterQuery } from "mongoose"
 import { successResponse } from "../helpers/responseHelper"
 import "dotenv/config";
 import { CarPartStockModels } from "../models/carPartStockModel";
+import { AdminUsers } from "../models/adminUserModel";
 
 
 //excel import
 const excelImport = async (file: any) => {
         const workbook: xlsx.WorkBook = xlsx.readFile(file.path)
-        console.log("w",workbook)
         const workSheet: xlsx.WorkSheet = workbook.Sheets[workbook.SheetNames[0]]
         const data: any [] = xlsx.utils.sheet_to_json(workSheet)
-        console.log("this is excel data",data)
         for(let i= 0; i < data.length; i++){
             let updateData:any = {}
+            let dealerResult
             updateData.totalQuantity = data[i].quantity
             updateData.originalPrice = data[i].originalPrice
+            updateData.discountPrice = data[i].discountPrice
             updateData.promotionPrice = data[i].promotionPrice
+            let dealerCode: string = data[i].dealerCode
+            if(dealerCode){
+                const dealerCodes: string = dealerCode.split(" (Suzuki)")[0]
+                dealerResult = await AdminUsers.findOne({code: dealerCodes})
+            }
             const partNumber = data[i].partNumber.split(" (Suzuki)")[0]
-            const result = await CarPartStockModels.updateMany({partNumber: partNumber},updateData)
+            let query: Partial<{ isDeleted: boolean, partNumber: string, dealerId: mongoose.Types.ObjectId }> = { isDeleted: false }
+            partNumber ? query.partNumber = partNumber : ""
+            dealerResult ? query.dealerId  = new mongoose.Types.ObjectId(dealerResult._id as mongoose.Types.ObjectId) : ""
+            const result = await CarPartStockModels.updateMany(query,updateData)
         }
         return successResponse({statusCode: 200, message: "Excel Imported Successfully.", data: "Excel Data"})
 }
