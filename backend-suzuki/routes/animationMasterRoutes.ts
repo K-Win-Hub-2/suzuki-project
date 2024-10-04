e3faee1c6fdf6726b9f6e34ca380a85f7e2745b8
Aung Ye Htet <aung.atg1738@gmail.com>
2024-10-04 09:28:21 +0630

    Drop: VerfiyToken for Animation Master

--- backend-suzuki/routes/animationMasterRoutes.ts
+++ backend-suzuki/routes/animationMasterRoutes.ts
@@ -1,18 +1,33 @@
-import { Express } from "express"
-import { verifyToken } from "../middlewares/verifyToken"
-import { checkAdminType } from "../validators/authCheck"
-import catchError from "../lib/catchError"
-import S3UploadImage from "../lib/fileUploader"
-import { createAnimationMaster, deleteAnimationMaster, listAllAnimationMaster, readAnimationMaster, updateAnimationMaster } from "../controllers/animationMasterController"
+import { Express } from "express";
+import { verifyToken } from "../middlewares/verifyToken";
+import { checkAdminType } from "../validators/authCheck";
+import catchError from "../lib/catchError";
+import S3UploadImage from "../lib/fileUploader";
+import {
+  createAnimationMaster,
+  deleteAnimationMaster,
+  listAllAnimationMaster,
+  readAnimationMaster,
+  updateAnimationMaster,
+} from "../controllers/animationMasterController";
 
-module.exports = (app: Express): void =>{
-    app.route("/api/v1/animation-master")
-        .get(verifyToken, catchError(listAllAnimationMaster))
-        .post(verifyToken, checkAdminType, S3UploadImage.array("animation_master"), catchError(createAnimationMaster))
+module.exports = (app: Express): void => {
+  app
+    .route("/api/v1/animation-master")
+    .get(catchError(listAllAnimationMaster))
+    .post(
+      checkAdminType,
+      S3UploadImage.array("animation_master"),
+      catchError(createAnimationMaster)
+    );
 
-    app.route("/api/v1/animation-master/:id")
-       .put(verifyToken, checkAdminType, S3UploadImage.array("animation_master"),  catchError(updateAnimationMaster))
-       .get(verifyToken, catchError(readAnimationMaster))
-       .delete(verifyToken, checkAdminType, catchError(deleteAnimationMaster))
-       
-}
+  app
+    .route("/api/v1/animation-master/:id")
+    .put(
+      checkAdminType,
+      S3UploadImage.array("animation_master"),
+      catchError(updateAnimationMaster)
+    )
+    .get(catchError(readAnimationMaster))
+    .delete(checkAdminType, catchError(deleteAnimationMaster));
+};
