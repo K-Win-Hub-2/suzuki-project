55a1d8adf9725bb05322c736aca5a236132c659e
Aung Ye Htet <aung.atg1738@gmail.com>
2024-10-05 10:08:45 +0630

    Change File Size to 1GB Max

--- backend-suzuki/lib/fileUploader.ts
+++ backend-suzuki/lib/fileUploader.ts
@@ -1,54 +1,70 @@
-import multer from "multer"
-import multerS3 from "multer-s3"
-import s3 from "../config/s3FileServerConfig"
+import multer from "multer";
+import multerS3 from "multer-s3";
+import s3 from "../config/s3FileServerConfig";
 import "dotenv/config";
-import { Request } from 'express'
-import path from "path"
+import { Request } from "express";
+import path from "path";
 
 const S3Storage = multerS3({
-    s3: s3,
-    bucket: process.env.BUCKET_NAME as string,
-    metadata: (req: any, file: any, cb: any)  => {
-        console.log("me",file)
-        cb(null, {fieldname: file.fieldname})
-    },
-    key: (req: any, file: any, cb: any) => {
-        if(process.env.NODE_ENV === "development") {
-            console.log("file",file)
-        }
-        const fileName = Date.now() + "_" + file.fieldname + "_" + file.originalname;
-        cb(null, fileName);
+  s3: s3,
+  bucket: process.env.BUCKET_NAME as string,
+  metadata: (req: any, file: any, cb: any) => {
+    console.log("me", file);
+    cb(null, { fieldname: file.fieldname });
+  },
+  key: (req: any, file: any, cb: any) => {
+    if (process.env.NODE_ENV === "development") {
+      console.log("file", file);
     }
-})
+    const fileName =
+      Date.now() + "_" + file.fieldname + "_" + file.originalname;
+    cb(null, fileName);
+  },
+});
 
 function sanitizeFile(file: any, cb: any) {
-    // Define the allowed extension
-    const fileExts = [".xlsx", "xlsx",".png", ".jpg", ".jpeg", ".gif", ".mp4", ".mpeg-4",".mov", ".avi", ".wmv", ".avchd", ".webm", ".flv"];
-    console.log("this is original name ", path.extname(file.originalname))
-    // Check allowed extensions
-    const isAllowedExt = fileExts.includes(
-        path.extname(file.originalname.toLowerCase())
-    );
+  // Define the allowed extension
+  const fileExts = [
+    ".xlsx",
+    "xlsx",
+    ".png",
+    ".jpg",
+    ".jpeg",
+    ".gif",
+    ".mp4",
+    ".mpeg-4",
+    ".mov",
+    ".avi",
+    ".wmv",
+    ".avchd",
+    ".webm",
+    ".flv",
+  ];
+  console.log("this is original name ", path.extname(file.originalname));
+  // Check allowed extensions
+  const isAllowedExt = fileExts.includes(
+    path.extname(file.originalname.toLowerCase())
+  );
 
-    // Mime type must be an image
-    // const isAllowedMimeType = file.mimetype.startsWith("image/");
+  // Mime type must be an image
+  // const isAllowedMimeType = file.mimetype.startsWith("image/");
 
-    if (isAllowedExt) {
-        return cb(null, true); // no errors
-    } else {
-        // pass error msg to callback, which can be displaye in frontend
-        cb("Error: File type not allowed!");
-    }
+  if (isAllowedExt) {
+    return cb(null, true); // no errors
+  } else {
+    // pass error msg to callback, which can be displaye in frontend
+    cb("Error: File type not allowed!");
+  }
 }
 
 const S3UploadImage = multer({
-    storage: S3Storage,
-    fileFilter: (req: Request, file: any, callback: any) => {
-        sanitizeFile(file, callback)
-    },
-    limits: {
-        fileSize: 1024 * 1024 * 2 // 2mb file size
-    }
-})
+  storage: S3Storage,
+  fileFilter: (req: Request, file: any, callback: any) => {
+    sanitizeFile(file, callback);
+  },
+  limits: {
+    fileSize: 1024 * 1024 * 1024, // 2mb file size
+  },
+});
 
-export default S3UploadImage
+export default S3UploadImage;
