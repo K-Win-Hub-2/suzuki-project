972e9cf818672a11e38d976af87540af46b3eae4
Aung Ye Htet <aung.atg1738@gmail.com>
2024-10-21 15:36:26 +0630

    prettier: customer models

--- backend-suzuki/models/customerModel.ts
+++ backend-suzuki/models/customerModel.ts
@@ -1,91 +1,95 @@
-import mongoose, { Document, Schema, Model } from 'mongoose'
-import bcryptHelpers from '../helpers/bcryptHelper';
+import mongoose, { Document, Schema, Model } from "mongoose";
+import bcryptHelpers from "../helpers/bcryptHelper";
 
 interface Customer extends Document {
-    isDeleted: boolean;
-    name: string;
-    email: string;
-    password: string;
-    phone: string;
-    address: string;
-    date: Date;
-    gender: String;
-    isBanned: boolean;
-    bannedReason?: string;
+  isDeleted: boolean;
+  name: string;
+  email: string;
+  password: string;
+  phone: string;
+  address: string;
+  date: Date;
+  gender: String;
+  isBanned: boolean;
+  bannedReason?: string;
 }
 
-
 const CustomerSchema: Schema<Customer> = new Schema({
-    isDeleted: {
-        type: Boolean,
-        default: false,
-    },
-    name: {
-        type: String,
-        required: true,
-    },
-    gender: {
-        type: String,
-        enum: ['Male', 'Female', 'Other']
-    },
-    email: {
-        type: String,
-        required: true,
-        unique: true,
-    },
-    password: {
-        type: String,
-        required: true,
-    },
-    phone: {
-        type: String,
-        required: true,
-    },
-    address: {
-        type: String,
-        required: true,
-    },
-    date: {
-        type: Date,
-        default: Date.now,
-    },
-    isBanned: {
-        type: Boolean,
-        default: false,
-    },
-    bannedReason: {
-        type: String,
-    },
+  isDeleted: {
+    type: Boolean,
+    default: false,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  gender: {
+    type: String,
+    enum: ["Male", "Female", "Other"],
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: String,
+    required: true,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+  isBanned: {
+    type: Boolean,
+    default: false,
+  },
+  bannedReason: {
+    type: String,
+  },
 });
 
-CustomerSchema.pre<Customer>('save', async function (next) {
-    const emailValidation = bcryptHelpers.validateEmail(this.email);
-    const passwordValidation = bcryptHelpers.validatePassword(this.password);
+CustomerSchema.pre<Customer>("save", async function (next) {
+  const emailValidation = bcryptHelpers.validateEmail(this.email);
+  const passwordValidation = bcryptHelpers.validatePassword(this.password);
 
-    if (!emailValidation) {
-        return next(new Error('Email should contain @'));
-    }
+  if (!emailValidation) {
+    return next(new Error("Email should contain @"));
+  }
 
-    if (!passwordValidation) {
-        return next(new Error('Password should contain at least one character and one number'));
-    }
+  if (!passwordValidation) {
+    return next(
+      new Error("Password should contain at least one character and one number")
+    );
+  }
 
-    const password = this.password;
-    this.password = await bcryptHelpers.encrypt(password);
+  const password = this.password;
+  this.password = await bcryptHelpers.encrypt(password);
 
-    next();
+  next();
 });
 
-CustomerSchema.pre('findOneAndUpdate', async function (next) {
-    const query = this as mongoose.Query<any, Customer>
-    const update = query.getUpdate() as Partial<Customer>
-    if(update.password){
-        update.password = await bcryptHelpers.encrypt(update.password);
-        query.setUpdate(update);
-    }
-    next()
-})
+CustomerSchema.pre("findOneAndUpdate", async function (next) {
+  const query = this as mongoose.Query<any, Customer>;
+  const update = query.getUpdate() as Partial<Customer>;
+  if (update.password) {
+    update.password = await bcryptHelpers.encrypt(update.password);
+    query.setUpdate(update);
+  }
+  next();
+});
 
-const Customers: Model<Customer> = mongoose.model<Customer>('customers', CustomerSchema);
+const Customers: Model<Customer> = mongoose.model<Customer>(
+  "customers",
+  CustomerSchema
+);
 
 export default Customers;
