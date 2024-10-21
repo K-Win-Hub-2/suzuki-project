c7c3df90833c7b6b949f206b682eb6ac1ee7d8d8
Aung Ye Htet <aung.atg1738@gmail.com>
2024-10-21 19:31:05 +0630

    add verifyToken and write GET Method Role Categories

--- backend-suzuki/routes/roleCategoriesRoute.ts
+++ backend-suzuki/routes/roleCategoriesRoute.ts
@@ -1,13 +1,19 @@
 import { Express } from "express";
 import catchError from "../lib/catchError";
+import { verifyToken } from "../middlewares/verifyToken";
 
 import {
   createRole,
   createRoleOnly,
+  getRole,
 } from "../controllers/roleCategoriesController";
 
 module.exports = (app: Express): void => {
-  app.route("/api/v1/roleCategories").post(catchError(createRole));
+  app.route("/api/v1/roleCategories").post(verifyToken, catchError(createRole));
 
-  app.route("/api/v1/roleCategoriesOnly").post(catchError(createRoleOnly));
+  app
+    .route("/api/v1/roleCategoriesOnly")
+    .post(verifyToken, catchError(createRoleOnly));
+
+  app.route("/api/v1/roleCategories").get(verifyToken, catchError(getRole));
 };
